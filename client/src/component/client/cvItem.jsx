97f0e3502550768f8ec9b53cc0b5e5b710f7e17e
tosin <tohshine@gmail.com>
2019-11-01 17:02:03 +0100

    styling done

--- client/src/component/client/cvItem.jsx
+++ client/src/component/client/cvItem.jsx
@@ -34,27 +34,26 @@ const cvItem = ({ cv }) => {
               <strong>{title}</strong>
             </div>
             {/*  personal details  */}
-            <p style={textStyle}>
+            <div>
+            <small style={textStyle}>
               <i className="fas fa-mobile-alt"></i> {phone}
-            </p>
-            <p style={textStyle}>
+            </small>
+            </div>
+            
+            <small style={textStyle}>
               <i className="fas fa-envelope"></i> {email}
-            </p>
+            </small>
             <span className="hr" />
-
-            <strong style={text}>Skills</strong>
+              
+           <div> <strong style={text}>Skills</strong></div>
             <p style={textStyle}>{techSkills}</p>
             <span className="hr" />
-
-            <strong style={text}>Social</strong>
-            <p style={textStyle}>{social}</p>
+             
+            <div><strong style={text}>Social</strong></div>
+            <small style={textStyle}>{social}</small>
             <span className="hr" />
 
-            <Link
-              to="/profile"
-              className="btn btn-danger my-2"
-              style={textStyle}
-            >
+            <Link to="/profile" className="btn btn-danger" style={textStyle}>
               <span>
                 <i className="fab fa-github"></i>
               </span>{' '}
@@ -66,56 +65,56 @@ const cvItem = ({ cv }) => {
               Personal Summary
             </h3>
             <div className="card " style={{ background: 'white' }}>
-              <p>{pSummary}</p>
+              <small>{pSummary}</small>
             </div>
 
             <h3 className="text-primary" style={{ textAlign: 'left' }}>
               Key Skill And Competence
             </h3>
             <div className="card " style={{ background: 'white' }}>
-              <p>{kSkillCom}</p>
+              <small>{kSkillCom}</small>
             </div>
 
             <h3 className="text-primary" style={{ textAlign: 'left' }}>
               Technical Skills
             </h3>
             <div className="card " style={{ background: 'white' }}>
-              <p>{techSkills}</p>
+              <small>{techSkills}</small>
             </div>
 
             <h3 className="text-primary" style={{ textAlign: 'left' }}>
               Projects
             </h3>
             <div className="card " style={{ background: 'white' }}>
-              <p>{project}</p>
+              <small>{project}</small>
             </div>
 
             <h3 className="text-primary" style={{ textAlign: 'left' }}>
               Development Task
             </h3>
             <div className="card " style={{ background: 'white' }}>
-              <p>{devTask}</p>
+              <small>{devTask}</small>
             </div>
 
             <h3 className="text-primary" style={{ textAlign: 'left' }}>
               Work Experience
             </h3>
             <div className="card " style={{ background: 'white' }}>
-              <p>{workExp}</p>
+              <small>{workExp}</small>
             </div>
 
             <h3 className="text-primary" style={{ textAlign: 'left' }}>
               Educational And Qualifications
             </h3>
             <div className="card " style={{ background: 'white' }}>
-              <p>{institution}</p>
+              <small>{institution}</small>
             </div>
 
             <h3 className="text-primary" style={{ textAlign: 'left' }}>
               Past Achievements
             </h3>
             <div className="card " style={{ background: 'white' }}>
-              <p>{pastAchieve}</p>
+              <small>{pastAchieve}</small>
             </div>
           </div>
         </div>
@@ -136,7 +135,8 @@ const textStyle = {
   marginLeft: '10px',
   marginTop: '10px',
   fontSize: '0.8em',
-  color: 'white'
+  color: 'white',
+  textAllign:'center'
 };
 
 export default cvItem;
