72a5b6bcac226979e6ece80bdb7895e023360e13
tosin <tohshine@gmail.com>
2019-11-05 15:29:16 +0100

    added update on reset password

--- client/src/component/pages/index.jsx
+++ client/src/component/pages/index.jsx
@@ -5,39 +5,42 @@ import { Link } from 'react-router-dom';
 const index = () => {
   return (
     <Fragment>
-      <Navbar />
-      <div className="container all-center vh">
-        <div className="grid-2">
-          <div>
-            <h1>
-              Brief introduction About myself and my working{' '}
-              <span className="text-primary">Experience</span>{' '}
-            </h1>
-            <p className="my-2">
-              Additional information will be provided upon request and i will be
-              glad to hear back from you.
-            </p>
-            <div>
-              <img
-                src="siteImg/whatapp.png"
-                style={{ width: '200px', height: '200px' }}
-                alt=""
-              />
-            </div>
+      <div className="vh">
+        <Navbar />
+        <div className="container all-center  ">
+          <div className="grid-2">
+            <div className="m-3">
+              <h1>
+                Brief introduction About myself and my working{' '}
+                <span className="text-primary">Experience</span>{' '}
+              </h1>
+              <p className="my-2">
+                Additional information will be provided upon request and i will
+                be glad to hear back from you.
+              </p>
+              <div>
+                <img
+                  src="siteImg/whatapp.png"
+                  style={{ width: '200px', height: '200px' }}
+                  alt=""
+                />
+              </div>
 
-            <Link to="/home">
-              <a href="" className="btn btn-block btn-primary my-2">
-                Goto CV
-              </a>
-            </Link>
-          </div>
-          <div>
-            <div className=" card bg-light img-centered">
-              <img src="siteImg/resume.svg" alt="" />
+              <Link to="/home">
+                <a href="" className="btn btn-block btn-primary my-2">
+                  Goto CV
+                </a>
+              </Link>
+            </div>
+            <div className="m-3">
+              <div className="m-3">
+                <img src="siteImg/resume.svg" alt="" />
+              </div>
             </div>
           </div>
         </div>
       </div>
+     
     </Fragment>
   );
 };
